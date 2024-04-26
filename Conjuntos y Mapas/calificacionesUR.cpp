53a28c08f7cbefee101d9ac2fcde9ea4ad3a939d
MariaValerianoo <149009830+MariaValerianoo@users.noreply.github.com>
2024-04-26 00:39:42 -0500

    update

--- Conjuntos y Mapas/calificacionesUR.cpp
+++ Conjuntos y Mapas/calificacionesUR.cpp
@@ -65,34 +65,66 @@ int main() {
     RegistroNotas registro;
     double nota;
     string materia, nom;
+    int op;
     
     do{
         cout<<"Bienvenido al sistema de notas Universidad del Rosario"<<endl;
         cout<<"Este es el menu de opciones que tenemos para ti: "<<endl;
         cout<<"1)Registro datos estudiante "<<endl;
-        cout<<"2) "<<endl;
-        cout<<"3)Registro datos estudiante "<<endl;
-        cout<<"4)Salir "<<endl;
+        cout<<"2)Mostrar nota de un estudiante en asignatura especifica "<<endl;
+        cout<<"3)Mostrar promedio general de un estudiante "<<endl;
+        cout<<"4)Mostrar promedio en una materia de todos los estudiantes "<<endl;
+        cout<<"5)Salir "<<endl;
+        cin>>op;
+
+        switch (op)
+        {
+        case 1:
+        //Registrar estudiantes
+        do
+        {
+            cout<<"Bienvenido "<<endl;
+            cout<<"Ingresa tu nombre, sin espacios: "<<endl;
+            cin>>nom;
+            cout<<"Ingresa la materia: "<<endl;
+            cin>>materia;
+            cout<<"Ingresa la nota: "<<endl;
+            cin>>nota;
+            registrarNota(registro, nom, materia,nota);
+            cout<<"Desea registrar otro usuario? \n 1)Si \n 2)No \n";
+            cin>>op;
+        } while (op!=2);
+        break;
+        case 2:
+            // Mostrar la nota de un estudiante en una asignatura específica
+            cout<<"Si deseas ver la nota de un estudiante"<<endl;
+            cout<<"Ingresa el nombre: "<<endl;
+            cin>>nom;
+            cout<<"Ingresa la materia: "<<endl;
+            cin>>materia;
+            mostrarNota(registro, nom,materia);
+            break;
+        case 3:
+            // Calcular el promedio de notas de un estudiante en todas las asignaturas
+            cout<<"Para ver el promedio, dime el nombre del estudiante: "<<endl;
+            cin>>nom;
+            cout << "Promedio de notas de "<<nom <<" : " << promedioEstudiante(registro, nom) << endl;
+            break;
+        case 4:
+        // Calcular el promedio de notas de una asignatura para todos los estudiantes
+            cout<<"Dime la materia de la cual quieres ver el promedio: "<<endl;
+            cin>>materia;
+            cout << "Promedio de notas de "<<materia <<" : " << promedioAsignatura(registro, materia) << endl;
 
 
-    }while(!=4)
+            break;
+        default:
+        cout<<"Opcion invalida vuelvelo a intentar:("<<endl;
+        break;
+        }
 
-    // Registrar algunas notas
-    registrarNota(registro, "Juan", "Matemáticas", 4.5);
-    registrarNota(registro, "Juan", "Física", 3.8);
-    registrarNota(registro, "María", "Matemáticas", 4.0);
-    registrarNota(registro, "María", "Física", 4.2);
-    registrarNota(registro, "Pedro", "Matemáticas", 3.9);
-    registrarNota(registro, "Pedro", "Química", 4.1);
+    }while(op!=5);
 
-    // Mostrar la nota de un estudiante en una asignatura específica
-    mostrarNota(registro, "Juan", "Matemáticas");
-
-    // Calcular el promedio de notas de un estudiante en todas las asignaturas
-    cout << "Promedio de notas de Juan: " << promedioEstudiante(registro, "Juan") << endl;
-
-    // Calcular el promedio de notas de una asignatura para todos los estudiantes
-    cout << "Promedio de notas de Matemáticas: " << promedioAsignatura(registro, "Matemáticas") << endl;
 
     return 0;
 }
